10845eeaf35029681c316dba050a342f76e70827
yuanwy <ywy32@qq.com>
2019-11-19 15:09:37 +0800

    9.0.2

--- templates/general.js
+++ templates/general.js
@@ -9,10 +9,7 @@ module.exports = [{
       manualInstallation += `
 #### iOS
 
-1. In XCode, in the project navigator, right click \`Libraries\` ➜ \`Add Files to [your project's name]\`
-2. Go to \`node_modules\` ➜ \`${moduleName}\` and add \`${name}.xcodeproj\`
-3. In XCode, in the project navigator, select your project. Add \`lib${name}.a\` to your project's \`Build Phases\` ➜ \`Link Binary With Libraries\`
-4. Run your project (\`Cmd+R\`)<
+1. In iOS path  excute $ pod update
 `;
     }
 
@@ -30,7 +27,7 @@ module.exports = [{
   	\`\`\`
 3. Insert the following lines inside the dependencies block in \`android/app/build.gradle\`:
   	\`\`\`
-      compile project(':${moduleName}')
+      implementation project(':${moduleName}')
   	\`\`\`
 `;
     }
